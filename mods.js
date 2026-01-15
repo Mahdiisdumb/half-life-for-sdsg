0bb49b15252c7a343ace69a1c227fe32156418c1
Mahdiisdumb <mahdihamade6789010@outlook.com>
2026-01-15 18:40:48 -0500

    bhugbvyuih

--- mods.js
+++ mods.js
@@ -1,30 +1,14 @@
 zipMods = [
   ['https://github.com/Mahdiisdumb/half-life-for-sdsg/releases/download/v1.0.0/valve.zip',   'Half-Life',       214283501],
-  ['https://github.com/Mahdiisdumb/half-life-for-sdsg/releases/download/v1.0.0/bshift.zip',  'Blue Shift',      304000000],
-  ['https://github.com/Mahdiisdumb/half-life-for-sdsg/releases/download/v1.0.0/gearbox.zip', 'Opposing Force',  221191140]
 ];
-
-pkgMods = [];
-
-var selectZip = document.getElementById('selectZip');
-var iArgs = document.getElementById('iArgs');
-
-selectZip.addEventListener('change', function () {
-  switch (selectZip.value) {
-    case 'Half-Life':
-      iArgs.value = '-dev 1 -game valve';
-      break;
-
-    case 'Blue Shift':
-      iArgs.value = '-dev 1 -game bshift';
-      break;
-
-    case 'Opposing Force':
-      iArgs.value = '-dev 1 -game gearbox';
-      break;
-
-    default:
-      iArgs.value = '';
-      break;
-  }
+pkgMods = [
+];
+var selectZip=document.getElementById('selectZip');
+var iArgs=document.getElementById('iArgs');
+selectZip.addEventListener('change', function(){
+	if(selectZip.value=="https://github.com/Mahdiisdumb/half-life-for-sdsg/releases/download/v1.0.0/valve.zip")
+	{
+		alert('Bugs: menu lagging.');
+		iArgs.value="-dev 1 -game valve";
+	}
 });
